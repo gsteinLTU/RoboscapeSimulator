9c8c33b2d144856a8e0ea4348d39e1c3387eb465
Gordon Stein <gstein@ltu.edu>
2020-01-06 16:33:30 -0600

    Heartbeat and listener

--- src/robots/Robot.js
+++ src/robots/Robot.js
@@ -24,18 +24,39 @@ class Robot {
 
         // Connect to RoboScape server to get commands
         this.socket = dgram.createSocket('udp4');
-        let initMsg = Buffer.alloc(11);
-        let macParts = this.mac.split(':');
-        for (let i in macParts) {
-            initMsg.writeUInt8(Number.parseInt(macParts[i], 16), i);
-        }
-        initMsg.writeUInt32BE(process.uptime(), 6);
-        initMsg.write('I', 10);
+        this.socket.on('message', this.msgHandler.bind(this));
 
-        // Tell server robot is alive
-        this.socket.send(initMsg, this.settings.port, this.settings.server);
+        // Start heartbeat
+        this.heartbeatInterval = setInterval(this.sendToServer.bind(this, 'I'), 1000);
 
         console.log(`Robot with MAC ${this.mac} created`);
+    }
+
+    /**
+     * Send a message as this robot to the server
+     * @param {String | Buffer} msg Message to send
+     */
+    sendToServer(msg) {
+        let msgBuff = Buffer.alloc(10 + msg.length);
+        let macParts = this.mac.split(':');
+        for (let i in macParts) {
+            msgBuff.writeUInt8(Number.parseInt(macParts[i], 16), i);
+        }
+        msgBuff.writeUInt32BE(process.uptime(), 6);
+        msgBuff.write(msg, 10);
+        // Tell server robot is alive
+        this.socket.send(msgBuff, this.settings.port, this.settings.server);
+    }
+
+    /**
+     * Handle an incoming message
+     * @param {Buffer} msg Message from server to this robot
+     */
+    msgHandler(msg) {
+        // Set speed command
+        if (String.fromCharCode(msg[0]) == 'S') {
+            // Apply force
+        }
     }
 }
 
