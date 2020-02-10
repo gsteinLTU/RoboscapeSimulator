f1d936e34d951b6329f181adb2f356ab62fe7cfe
Gordon Stein <gstein@ltu.edu>
2020-02-10 12:27:14 -0600

    Better estimate of distance

--- src/robots/ParallaxRobot.js
+++ src/robots/ParallaxRobot.js
@@ -101,20 +101,24 @@ class ParallaxRobot extends Robot {
 
         const upVec = Vector.rotate(Vector.create(0, 1), this.body.angle);
 
-        const rayStart = Vector.add(this.body.position, Vector.mult(upVec, 25));
+        const rayStart = Vector.add(this.body.position, Vector.mult(upVec, 1));
         const rayEnd = Vector.add(this.body.position, Vector.mult(upVec, 325));
 
         // Find objects in sensor path
-        let query = Matter.Query.ray(this.engine.world.bodies, rayStart, rayEnd);
+        let query = Matter.Query.ray(
+            this.engine.world.bodies.filter(b => !b.label.includes(this.body.label)),
+            rayStart,
+            rayEnd
+        );
 
         // Find closest object
         query = query.sort(r => Math.pow(r.bodyA.position.x - this.body.position.x, 2) + Math.pow(r.bodyA.position.y - this.body.position.y, 2));
-        let distance = 300;
+        const maxDist = 300;
+        let distance = maxDist;
         if (query !== undefined && query.length > 0) {
             let target = query[0].bodyA;
 
             let targetVerts = [...target.vertices];
-            targetVerts.push(targetVerts[0]);
             let targetEdges = [];
 
             // Get all edges
@@ -129,9 +133,9 @@ class ParallaxRobot extends Robot {
 
             // Test for intersections
             let x1 = rayStart.x;
-            let x2 = rayStart.x + upVec.x * 300;
+            let x2 = rayStart.x + upVec.x * maxDist;
             let y1 = rayStart.y;
-            let y2 = rayStart.y + upVec.y * 300;
+            let y2 = rayStart.y + upVec.y * maxDist;
             for (let edge of targetEdges) {
                 let x3 = edge.p1x;
                 let y3 = edge.p1y;
@@ -150,7 +154,7 @@ class ParallaxRobot extends Robot {
                     continue;
                 }
 
-                distance = Math.min(distance, t * 300);
+                distance = Math.max(4, Math.min(distance, t * maxDist) - this.body.height / 4);
             }
         }
 
