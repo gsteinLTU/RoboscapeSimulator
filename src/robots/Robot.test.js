e70273afeca44f436685c933bc81a711a6f385bb
Gordon Stein <gstein@ltu.edu>
2020-02-14 15:54:03 -0600

    Add robot tests

---
+++ src/robots/Robot.test.js
@@ -0,0 +1,29 @@
+const Robot = require('./Robot');
+const Matter = require('matter-js');
+const Engine = Matter.Engine;
+
+describe('Robot tests', () => {
+    let engine;
+    beforeAll(() => {
+        engine = engine = Engine.create();
+    });
+
+    test('random MAC should be used if none provided', () => {
+        let testRobot = new Robot(null, null, engine);
+        expect(testRobot.mac).toEqual(expect.stringMatching(/^([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}$/));
+    });
+
+    test('Correct MAC should be used if provided', () => {
+        let testRobot = new Robot('aa:aa:aa:aa:aa:aa', null, engine);
+        expect(testRobot.mac).toEqual('aa:aa:aa:aa:aa:aa');
+    });
+
+    test('Correct position should be used if provided', () => {
+        let randX = Math.random() * 1000;
+        let randY = Math.random() * 1000;
+
+        let testRobot = new Robot(null, { x: randX, y: randY }, engine);
+        expect(testRobot.body.position.x).toBeCloseTo(randX);
+        expect(testRobot.body.position.y).toBeCloseTo(randY);
+    });
+});
--- src/util.test.js
+++ src/util.test.js
@@ -1,5 +1,7 @@
 const util = require('./util');
 
-test('generateRandomMAC should match the correct format', () => {
-    expect(util.generateRandomMAC()).toEqual(expect.stringMatching(/^([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}$/));
+describe('generateRandomMAC tests', () => {
+    test('generateRandomMAC should match the correct format', () => {
+        expect(util.generateRandomMAC()).toEqual(expect.stringMatching(/^([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}$/));
+    });
 });
