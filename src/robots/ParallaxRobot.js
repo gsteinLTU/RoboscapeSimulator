0c91d25c6a5b1e5697b54971c204ed640d7423f9
Gordon Stein <gstein@ltu.edu>
2020-04-02 21:36:30 -0500

    Encoder Ticks (#14)
    * Send ticks to server
    
    * Update ticks while driving

--- src/robots/ParallaxRobot.js
+++ src/robots/ParallaxRobot.js
@@ -17,6 +17,10 @@ class ParallaxRobot extends Robot {
             settings.image = 'parallax_robot';
         }
 
+        if(settings.speedToTicks == undefined){
+            settings.speedToTicks = (15000 / 60);
+        }
+
         settings = _.defaults(settings, Robot.defaultSettings);
 
         super(mac, position, engine, { ...settings });
@@ -27,6 +31,10 @@ class ParallaxRobot extends Robot {
         // Setup LEDs
         this.body.ledStatus = [0, 0];
         this.commandHandlers['L'] = this.updateLEDs.bind(this);
+
+        // Setup ticks
+        this.ticks = {left: 0, right: 0};
+        this.commandHandlers['T'] = this.sendTicks.bind(this);
 
         // Update center of mass position
         this.body.positionPrev.x = this.mainBody.position.x - (this.body.position.x - this.body.positionPrev.x);
@@ -46,6 +54,15 @@ class ParallaxRobot extends Robot {
         super.createBody(position);
 
         this.addWhiskers(this.engine);
+    }
+
+    /**
+     * Applies force of wheels to robot
+     */
+    drive() {
+        this.ticks.left += this.setSpeed.left * this.settings.speedToTicks;
+        this.ticks.right += this.setSpeed.right * this.settings.speedToTicks;
+        super.drive();
     }
 
     /**
@@ -187,6 +204,20 @@ class ParallaxRobot extends Robot {
         if (led < this.body.ledStatus.length) {
             this.body.ledStatus[led] = command;
         }
+    }
+
+    /**
+     * Send ticks information to server
+     */
+    sendTicks() {
+        let temp = Buffer.alloc(9);
+        temp.write('T');
+
+        // Add ticks to message
+        temp.writeInt32LE(this.ticks.left,1);
+        temp.writeInt32LE(this.ticks.right,5);
+
+        this.sendToServer(temp);
     }
 }
 
