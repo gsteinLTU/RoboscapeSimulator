daf2b1bd8fc0f9ea55503f401d1464e9ca3244b8
Gordon <gstein@ltu.edu>
2020-04-04 17:02:54 -0500

    test that parallaxrobot has command handlers

--- src/robots/ParallaxRobot.test.js
+++ src/robots/ParallaxRobot.test.js
@@ -4,12 +4,22 @@ const Engine = Matter.Engine;
 
 describe('ParallaxRobot tests', () => {
     let engine;
+    let testRobot;
     beforeAll(() => {
         engine = engine = Engine.create();
+        testRobot = new ParallaxRobot(null, null, engine);
+    });
+
+    test('Has all expected command handlers', () => {
+        expect(testRobot.commandHandlers).toMatchObject(expect.objectContaining({
+            'L': expect.any(Function),
+            'R': expect.any(Function),
+            'S': expect.any(Function),
+            'T': expect.any(Function)
+        }));
     });
 
     test('Can set LEDs', () => {
-        let testRobot = new ParallaxRobot(null, null, engine);
         testRobot.commandHandlers['L'](Buffer.of('L', 0, 1));
         testRobot.commandHandlers['L'](Buffer.of('L', 1, 1));
         expect(testRobot.body.ledStatus[0]).toEqual(1);
