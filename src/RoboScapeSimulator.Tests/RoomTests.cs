697f1c0d71ade3bd452507af0cbe089edb29daba
Gordon Stein <gstein@ltu.edu>
2022-02-09 17:38:54 -0600

    Additional simple test

--- src/RoboScapeSimulator.Tests/RoomTests.cs
+++ src/RoboScapeSimulator.Tests/RoomTests.cs
@@ -1,13 +1,22 @@
-using RoboScapeSimulator;
 using Xunit;
-namespace RoboScapeSimulatorTests;
+namespace RoboScapeSimulator.Tests;
 
 public class RoomTests
 {
     [Fact]
     public void CreateRoom()
     {
-        Room testRoom = new Room("testRoom");
+        Room testRoom = new("testRoom");
         Assert.Equal("testRoom", testRoom.Name);
+    }
+
+    [Fact]
+    public void RoomTimeTest()
+    {
+        Room testRoom = new("testRoom");
+        testRoom.Update(1.0f / 60.0f);
+        Assert.InRange(testRoom.Time, 1.0f / 60.0f - float.Epsilon, 1.0f / 60.0f + float.Epsilon);
+        testRoom.Update(1.0f / 60.0f);
+        Assert.InRange(testRoom.Time, 2.0f / 60.0f - float.Epsilon, 2.0f / 60.0f + float.Epsilon);
     }
 }
