e34a5d37d77bf632754eaa4a0837840803ce51d4
Gordon Stein <gstein@ltu.edu>
2022-04-04 11:20:25 -0500

    Refactor and add overlap to walls

--- src/RoboScapeSimulator/Environments/LIDARRoadEnvironment.cs
+++ src/RoboScapeSimulator/Environments/LIDARRoadEnvironment.cs
@@ -36,11 +36,9 @@ namespace RoboScapeSimulator.Environments
             // Ground
             var ground = new Ground(room, visualInfo: new VisualInfo() { Color = "#222" });
 
-            // Demo robot
-            var robot = new ParallaxRobot(room, new(0, 0.25f, 0), Quaternion.Identity);
-            var lidar = new LIDARSensor(robot) { Offset = new(0, 0.1f, 0.07f), NumRays = 3, MinAngle = -2 * MathF.PI / 6f + MathF.PI / 2, MaxAngle = 2f * MathF.PI / 6 + MathF.PI / 2, MaxDistance = 5 };
-            lidar.Setup(room);
 
+            float endPosZ = 7;
+            float endPosX = 0;
             switch (_courseType)
             {
                 case Courses.Hard:
@@ -79,10 +77,8 @@ namespace RoboScapeSimulator.Environments
                         new(0.5f, 0, 0f),
                     });
 
-
-                    // Start and end areas
-                    var hardstart = new Cube(room, 1, 0.01f, 1, new(0, 0.005f, 0f), Quaternion.CreateFromYawPitchRoll(0, 0.05f, 0), isKinematic: true, visualInfo: new VisualInfo() { Color = "#D22" });
-                    var hardend = new Cube(room, 1, 0.01f, 1, new(2f, 0.005f, 8f), Quaternion.CreateFromYawPitchRoll(0, -0.05f, 0), isKinematic: true, visualInfo: new VisualInfo() { Color = "#2D2" });
+                    endPosZ = 8;
+                    endPosX = 2;
 
                     break;
                 case Courses.Easy:
@@ -120,14 +116,17 @@ namespace RoboScapeSimulator.Environments
                         new(0.5f, 0, -0.5f),
                         new(0.5f, 0, 0f),
                     });
-
-
-                    // Start and end areas
-                    var easystart = new Cube(room, 1, 0.01f, 1, new(0, 0.005f, 0f), Quaternion.CreateFromYawPitchRoll(0, 0.05f, 0), isKinematic: true, visualInfo: new VisualInfo() { Color = "#D22" });
-                    var easyend = new Cube(room, 1, 0.01f, 1, new(0, 0.005f, 7f), Quaternion.CreateFromYawPitchRoll(0, -0.05f, 0), isKinematic: true, visualInfo: new VisualInfo() { Color = "#2D2" });
-
                     break;
             }
+
+            // Start and end areas
+            var start = new Cube(room, 1, 0.01f, 1.1f, new(0, 0.005f, 0f), Quaternion.CreateFromYawPitchRoll(0, 0.005f, 0), isKinematic: true, visualInfo: new VisualInfo() { Color = "#D22" });
+            var end = new Cube(room, 1, 0.01f, 1.1f, new(endPosX, 0.005f, endPosZ), Quaternion.CreateFromYawPitchRoll(0, -0.005f, 0), isKinematic: true, visualInfo: new VisualInfo() { Color = "#2D2" });
+
+            // Demo robot
+            var robot = new ParallaxRobot(room, new(0, 0.25f, 0), Quaternion.Identity);
+            var lidar = new LIDARSensor(robot) { Offset = new(0, 0.1f, 0.07f), NumRays = 3, MinAngle = -2 * MathF.PI / 6f + MathF.PI / 2, MaxAngle = 2f * MathF.PI / 6 + MathF.PI / 2, MaxDistance = 5 };
+            lidar.Setup(room);
 
             static void AddPath(Room room, List<Vector3> points, float thickness = 0.1f, float height = 0.5f)
             {
@@ -139,18 +138,18 @@ namespace RoboScapeSimulator.Environments
                 }
             }
 
-            static void AddWall(Room room, Vector3 p1, Vector3 p2, float thickness = 0.1f, float height = 0.5f)
+            static Cube? AddWall(Room room, Vector3 p1, Vector3 p2, float thickness = 0.1f, float height = 0.5f, float padding = 0.025f)
             {
                 var length = (p2 - p1).Length();
 
                 if (length < 0.0001)
                 {
-                    return;
+                    return null;
                 }
 
                 var center = p1 + ((p2 - p1) * 0.5f);
                 var angle = MathF.Atan2(p2.Z - p1.Z, p2.X - p1.X);
-                var wall1 = new Cube(room, length, height, thickness, center, Quaternion.CreateFromAxisAngle(Vector3.UnitY, -angle), true, visualInfo: new VisualInfo() { Color = "#633" });
+                return new Cube(room, length + padding, height, thickness, center, Quaternion.CreateFromAxisAngle(Vector3.UnitY, -angle), true, visualInfo: new VisualInfo() { Color = "#633" });
             }
 
         }
