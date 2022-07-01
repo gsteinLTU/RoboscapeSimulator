68384c63012528b9b6b14cc4ac4c1272c06c05c9
Gordon Stein <gstein@ltu.edu>
2022-07-01 15:44:27 -0500

    Partially working demo

--- src/RoboScapeSimulator/Environments/TreasureHuntEnvironment.cs
+++ src/RoboScapeSimulator/Environments/TreasureHuntEnvironment.cs
@@ -7,18 +7,17 @@ using RoboScapeSimulator.IoTScape;
 using RoboScapeSimulator.IoTScape.Devices;
 namespace RoboScapeSimulator.Environments
 {
-    class TreasureHuntEnvironment : EnvironmentConfiguration
+    internal class TreasureHuntEnvironment : EnvironmentConfiguration
     {
         public TreasureHuntEnvironment()
         {
             Name = "Treasure Hunt";
             ID = "treasurehunt";
-            Description = "RadiationSensor Demo Environment";
+            Description = "treasureSensor Demo Environment";
         }
 
-        IoTScapeObject? radiationSensor;
-
-        PositionSensor? locationSensor;
+        private IoTScapeObject? treasureSensor;
+        private PositionSensor? locationSensor;
 
         public override object Clone()
         {
@@ -30,20 +29,29 @@ namespace RoboScapeSimulator.Environments
             Trace.WriteLine($"Setting up {Name} environment");
 
             // Ground
-            var ground = new Ground(room);
+            Ground ground = new(room);
 
             // Walls
             EnvironmentUtils.MakeWalls(room);
 
             // Demo robot 
-            var robot = new ParallaxRobot(room, debug: false);
+            ParallaxRobot robot = new(room, new Vector3(0, 0.25f, 0), Quaternion.Identity);
 
-            var barrel = new Cube(room, initialPosition: new Vector3(3, 0, 3), initialOrientation: Quaternion.Identity, visualInfo: new VisualInfo() { ModelName = "chest_opt.glb", ModelScale = 0.4f }, isKinematic: true);
+            Cube chest = new(room, 0.5f, 0.5f, 0.5f, initialPosition: new Vector3(1, -1.5f, 1), initialOrientation: Quaternion.Identity, visualInfo: new VisualInfo() { ModelName = "chest_opt.glb", ModelScale = 0.4f }, isKinematic: true);
+            Trigger chestTrigger = new(room, chest.Position + new Vector3(0, 2f, 0), Quaternion.Identity, 0.5f, 1f, 0.5f, oneTime: true);
+
+            chestTrigger.OnTriggerEnter += (o, e) =>
+            {
+                if (e == robot)
+                {
+                    chest.Position += new Vector3(0, 1.5f, 0);
+                }
+            };
 
             locationSensor = new(robot);
             locationSensor.Setup(room);
 
-            IoTScapeServiceDefinition radiationSensorDefinition = new(
+            IoTScapeServiceDefinition treasureSensorDefinition = new(
                 "MetalDetector",
                 new IoTScapeServiceDescription() { version = "1" },
                 "",
@@ -60,11 +68,11 @@ namespace RoboScapeSimulator.Environments
                 new Dictionary<string, IoTScapeEventDescription>());
 
             var sensorBody = robot.BodyReference;
-            var targetBody = barrel.BodyReference;
-            var targetIntensity = 100;
+            var targetBody = chest.BodyReference;
+            int targetIntensity = 100;
 
-            radiationSensor = new(radiationSensorDefinition, robot.ID);
-            radiationSensor.Methods["getIntensity"] = (string[] args) =>
+            treasureSensor = new(treasureSensorDefinition, robot.ID);
+            treasureSensor.Methods["getIntensity"] = (string[] args) =>
             {
                 float intensity = 0;
                 float distance = (sensorBody.Pose.Position - targetBody.Pose.Position).LengthSquared();
@@ -73,8 +81,12 @@ namespace RoboScapeSimulator.Environments
                 return new string[] { intensity.ToString() };
             };
 
-            radiationSensor.Setup(room);
+            treasureSensor.Setup(room);
 
+            room.OnReset += (r, e) =>
+            {
+                chestTrigger.Reset();
+            };
         }
     }
 }
