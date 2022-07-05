baaf05d1c9d4645083115cf9f8a5f56df199f63d
Gordon Stein <gstein@ltu.edu>
2022-07-05 14:54:26 -0500

    Refix hibernate

--- src/RoboScapeSimulator/Room.cs
+++ src/RoboScapeSimulator/Room.cs
@@ -543,7 +543,7 @@ namespace RoboScapeSimulator
             dt *= TimeMultiplier;
 
             // Check if too much time has passed
-            if ((LastInteractionTime - Program.StartTicks) / 1000f > Timeout)
+            if ((Environment.TickCount64 - LastInteractionTime) / 1000f > Timeout)
             {
                 // Go to sleep
                 Hibernating = true;
